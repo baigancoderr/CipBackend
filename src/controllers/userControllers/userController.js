22ed69f88c3af2f53033607959051179a9615dc7
Gouri shankar <gouribahetwar0.com>
2026-04-06 13:40:35 +0530

    done

--- src/controllers/userControllers/userController.js
+++ src/controllers/userControllers/userController.js
@@ -2435,7 +2435,7 @@ const createDeposit = async (req, res) => {
     if (!userId) {
       return res.status(400).json({ 
         success: false, 
-        message: "userId is required" 
+        message: "userId is required ." 
       });
     }
 
