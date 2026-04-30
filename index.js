a16795d63c413532ae0f777fed4e3ad472125f46
aladin <aladinx98@gmail.com>
2026-04-30 18:53:47 +0530

    cors add

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ const corsOptions = {
        "http://localhost:5173",
        "http://localhost:5174",
       "https://web.telegram.org",
+      "https://cipera.net/",
+      "http://app.cipera.net/",
+      "https://backend.cipera.net/",
+      "https://admin.cipera.net/",
 
       "https://t.me/cipera_bot/direct",
       "https://t.me/cipera_bot",
